e6d07920e02c94c09b002878da8f391cba5b76de
kknb1982 <kirstin.barnett@gmail.com>
2025-02-21 11:26:38 +0000

    Updated assignment 3

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -7,9 +7,13 @@ import json
 
 url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
 
-response = requests.get(url)
-with open("cso.json", "wt") as fp:
-    print(json.dumps(response.json()), file=fp)
+def getAll():
+    response = requests.get(url)
+    return response.json()
+
+if __name__ == "__main__":
+    with open("cso.json", "wt") as fp:
+        print(json.dumps(getAll()), file=fp)
 
 
 
