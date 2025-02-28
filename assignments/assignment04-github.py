05832b56f23c0d973f26b3be254197108b40eab8
kknb1982 <kirstin.barnett@gmail.com>
2025-02-28 13:28:52 +0000

    assignment 4 - APIs

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -1,25 +1,22 @@
 # Read in a file from a repository, replace all instances of the word "Andrew" with "Kirstin", and write the modified file back to the repository.
 # Author: Kirstin Barnett
 
-from github import Github
 import requests
 from config import apikeys as cfg
 
-apikey = cfg["githubkey"]
+apikey = cfg["apikeys"]
 
-g = Github(apikey)
-
+url = "https://github.com/kknb1982/WSAA-coursework/mywork"
+filename = "classlist.txt"
 
 # Get the file from the repository
-url = "https://raw.githubusercontent.com/AndrewFinnell/IS211_Assignment4/master/README.md"
-response = requests.get(url)
+response = requests.get(url, auth=('token', apikey))
 response.raise_for_status() # Check for errors
 
 # Replace all instances of "Andrew" with "Kirstin"
 modified_text = response.text.replace("Andrew", "Kirstin")  
 
 # Write the modified file back to the repository
-url = "https://raw.githubusercontent.com/AndrewFinnell/IS211_Assignment4/master/README.md"
 response = requests.put(url, data=modified_text)
 response.raise_for_status() # Check for errors
 print("File has been modified and written back to the repository.")
