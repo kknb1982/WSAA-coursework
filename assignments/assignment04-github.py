f7077c02a65b21b4ba8e29e34ca26595f349c495
kknb1982 <kirstin.barnett@gmail.com>
2025-03-01 08:44:11 +0000

    updated code to read in file contents

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -2,22 +2,34 @@
 # Author: Kirstin Barnett
 
 import requests
+import json
 from config import apikeys as cfg
 
-apikey = cfg["apikeys"]
 
-url = "https://github.com/kknb1982/WSAA-coursework/mywork"
 filename = "classlist.txt"
+apikey = cfg["apikeys"]
+url = "https://github.com/kknb1982/WSAA-coursework/"
 
-# Get the file from the repository
-response = requests.get(url, auth=('token', apikey))
-response.raise_for_status() # Check for errors
+# Access the repository
+response = requests.get(url, auth = ('token', apikey))
+print (response.status_code)
 
-# Replace all instances of "Andrew" with "Kirstin"
-modified_text = response.text.replace("Andrew", "Kirstin")  
+# Access the file from the repository
+with open(filename, 'r') as fp:
+    content = fp.read()
+    print("File has been read from the repository.")
+
+# Search for "Andrew" and replace with "Kirstin" in the JSON data
+updatedtxt = content.replace("Andrew", "Kirstin")
+print("Text has been modified.")
+
+with open (filename, "w") as fp:
+    fp.write(updatedtxt)
+    fp.close()
+
+print("File has been searched and modified where needed.")
 
 # Write the modified file back to the repository
-response = requests.put(url, data=modified_text)
-response.raise_for_status() # Check for errors
+response = requests.put(url,  auth=('token', apikey))
 print("File has been modified and written back to the repository.")
 
