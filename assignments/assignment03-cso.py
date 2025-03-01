bc95577d20c5d6206e51d6441ec70d8516c50287
kknb1982 <kirstin.barnett@gmail.com>
2025-03-01 08:23:43 +0000

    annotated code

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -5,12 +5,15 @@
 import requests
 import json
 
+# URL for the dataset
 url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
 
+# Function to get the dataset
 def getAll():
     response = requests.get(url)
     return response.json()
 
+# Write the dataset to a file
 if __name__ == "__main__":
     with open("cso.json", "wt") as fp:
         print(json.dumps(getAll()), file=fp)
