454cebbaef365435c9a5e8ebd01053d3797f9f11
Kirstin Barnett <123597290+kknb1982@users.noreply.github.com>
2025-03-29 09:34:01 +0000

    updated app routes

--- mywork/server.py
+++ mywork/server.py
@@ -2,10 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/books')
+def getall():
+    return "Get all books!"
 
 @app.route('/hello')
 def hello():
